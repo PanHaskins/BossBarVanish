a47fd3cb5cba29b643f36bda5a914c8b4c38dd2a
PanHaskins <Haskins123@centrum.sk>
2023-07-27 19:06:50 +0200

    Using API instead of Bukkit Listeners

--- src/main/java/sk/panhaskins/bossbarvanish/VanishPlugins/VelocityVanishPlugin.java
+++ src/main/java/sk/panhaskins/bossbarvanish/VanishPlugins/VelocityVanishPlugin.java
@@ -1,14 +1,10 @@
 package sk.panhaskins.bossbarvanish.VanishPlugins;
 
-import de.myzelyam.api.vanish.VanishAPI;
-import net.achymake.simplevanish.SimpleVanish;
+import ir.syrent.velocityvanish.spigot.event.PreUnVanishEvent;
+import ir.syrent.velocityvanish.spigot.event.PreVanishEvent;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerCommandPreprocessEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.metadata.MetadataValue;
 import sk.panhaskins.bossbarvanish.BossBarVanish;
 
 public class VelocityVanishPlugin implements Listener {
@@ -20,68 +16,17 @@ public class VelocityVanishPlugin implements Listener {
     }
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
+    public void offVanish(PreUnVanishEvent e){
         Player player = e.getPlayer();
-        if(isVanished(player)){
-            plugin.bar.addPlayer(player);
+        if (plugin.bar.hasBar(player)) {
+            plugin.bar.removePlayer(player);
         }
     }
 
     @EventHandler
-    public void onLeft(PlayerQuitEvent e) {
+    public void onVanish(PreVanishEvent e){
         Player player = e.getPlayer();
-        if(isVanished(player)){
-            if(plugin.bar.hasBar(player)){
-                plugin.bar.removePlayer(player);
-            }
-        }
+        plugin.bar.addPlayer(player);
     }
 
-
-    @EventHandler
-    public void usedVanishCommand(PlayerCommandPreprocessEvent e) {
-        Player player = e.getPlayer();
-
-        if (e.getMessage().equalsIgnoreCase("/vanish")) {
-
-            if (player.hasPermission("velocityvanish.command.vanish")) {
-                if (!isVanished(player)) {
-                    plugin.bar.addPlayer(player);
-                } else {
-                    if (plugin.bar.hasBar(player)) {
-                        plugin.bar.removePlayer(player);
-                    }
-                }
-            }
-        }
-
-        if(e.getMessage().toLowerCase().startsWith("/vanish ")){
-            String[] args = e.getMessage().split(" ");
-
-            if(args.length > 0){
-                Player target = plugin.getServer().getPlayerExact(args[1]);
-                if(target == null) {
-                    return;
-                }
-                if (player.hasPermission("velocityvanish.command.vanish")) {
-                    if (!isVanished(player)) {
-                        plugin.bar.addPlayer(target);
-                    } else {
-                        if (plugin.bar.hasBar(target)) {
-                            plugin.bar.removePlayer(target);
-                        }
-                    }
-                }
-
-            }
-        }
-
-    }
-
-    public boolean isVanished(Player player) {
-        for (MetadataValue meta : player.getMetadata("vanished")) {
-            if (meta.asBoolean()) return true;
-        }
-        return false;
-    }
 }
